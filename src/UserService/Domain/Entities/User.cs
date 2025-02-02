8f2d0f10be073ac231ef155f8c74fe30b36bce85
QuantMis <realhost01@gmail.com>
2025-02-02 21:21:48 +0800

    Added db connection, controllers and entity

--- src/UserService/Domain/Entities/User.cs
+++ src/UserService/Domain/Entities/User.cs
@@ -7,9 +7,11 @@ namespace UserService.Domain.Entities
 {
     public class User
     {
-        public required int Id { get; set;}
-        public required string Name { get; set;}
-        public required string Email { get; set;}
-        public required DateTime CreatedAt { get; set;}
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string PasswordSalt { get; set; }
     }
 }
